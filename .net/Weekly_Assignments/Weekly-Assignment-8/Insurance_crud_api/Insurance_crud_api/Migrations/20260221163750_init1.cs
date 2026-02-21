33dc4794c7531eb1c1ef590d3731398794495f6d
Srinidhi <srinidhikommawar109@gmail.com>
2026-02-21 23:26:41 +0530

    Insurance Management Week-8

---
+++ .net/Weekly_Assignments/Weekly-Assignment-8/Insurance_crud_api/Insurance_crud_api/Migrations/20260221163750_init1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Insurance_crud_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class init1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
